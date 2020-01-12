eba3078b49976c735e5ccd2eaf203eacef4f60f7
Mad <github@typ03.de>
2020-01-12 11:13:44 +0100

    rewrite logger

--- Source/HeadshotWeaponLimit/HeadshotWeaponLimit.cs
+++ Source/HeadshotWeaponLimit/HeadshotWeaponLimit.cs
@@ -11,24 +11,24 @@ namespace HeadshotWeaponLimit
         internal static string LogPath;
         internal static string ModDirectory;
         internal static Settings Settings;
-
         // BEN: Debug (0: nothing, 1: errors, 2:all)
-        internal static int DebugLevel = 2;
+        internal static int DebugLevel = 1;
 
         public static void Init(string directory, string settings)
         {
             ModDirectory = directory;
-
             LogPath = Path.Combine(ModDirectory, "HeadshotWeaponLimit.log");
-            File.CreateText(HeadshotWeaponLimit.LogPath);
+
+            Logger.Initialize(LogPath, DebugLevel, ModDirectory, nameof(HeadshotWeaponLimit));
 
             try
             {
                 Settings = JsonConvert.DeserializeObject<Settings>(settings);
             }
-            catch (Exception)
+            catch (Exception e)
             {
                 Settings = new Settings();
+                Logger.LogError(e);
             }
 
             // Harmony calls need to go last here because their Prepare() methods directly check Settings...
